34719430dd0e1d2bc3879b1695269576e0985bfc
jessuss00 <jesusmadro23@gmail.com>
2025-04-22 13:15:27 +0200

    conexion con la api

--- pruebas_woocomerce/src/app/app.component.ts
+++ pruebas_woocomerce/src/app/app.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component , OnInit} from '@angular/core';
+import { ProductoService, Producto } from './services/producto.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'pruebas_woocomerce';
+export class AppComponent implements OnInit {
+  productos: Producto[] = [];
+  constructor(private productoService: ProductoService) {}
+  ngOnInit() {
+    this.productoService.obtenerProductos().subscribe(
+      productos => {
+        this.productos = productos;
+        console.log(productos);
+      },
+      error => {
+        console.error('Error al obtener productos', error);
+      }
+    );
+  }
+  
+  //title = 'pruebas_woocomerce';
 }
---
+++ pruebas_woocomerce/src/app/services/producto.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders  } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Producto {
+  id: number;
+  name: string;
+  price: number;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductoService {
+  private apiUrl ="https://jmadronal.moneomk.com/wp-json/wc/v3/products"
+  private username = 'ck_5d90c4a3aec40857441f11ed3adc96c47d73188a';
+  private password = 'cs_70d3026407bc5cfd02ed799a56339e30e61209b4';
+  constructor(private http: HttpClient) { }
+
+  obtenerProductos(): Observable<Producto[]> {
+    const auth = btoa(`${this.username}:${this.password}`);
+    const headers =new HttpHeaders({ 'Authorization': `Basic ${auth}` });
+
+    return this.http.get<Producto[]>(this.apiUrl, { headers });
+  }
+}
